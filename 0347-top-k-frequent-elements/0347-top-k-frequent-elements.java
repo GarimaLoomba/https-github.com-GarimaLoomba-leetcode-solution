df1a9ef95add5b3f226a143c861f3d0dfd8bd809
GarimaLoomba <loombagarima33@gmail.com>
2026-09-16 15:39:14 +0530

    Time: 13 ms (79.31%), Space: 47.7 MB (49.68%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,32 @@
 class Solution {
-
     class Pair{
         int element ;
-        int frequency ;
+        int freq ;
 
-        Pair(int element , int frequency ){
+        Pair(int element , int freq){
             this.element = element ;
-            this.frequency = frequency ;
+            this.freq = freq  ;
         }
     }
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer> map =  new HashMap<>() ;
+        HashMap<Integer ,Integer> map = new HashMap<>() ;
 
-        for(int i =0 ;i < nums.length ; i++){
+        for(int i =0 ; i < nums.length; i++){
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
 
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.frequency - a.frequency);
-        for(int num : map.keySet()){
-            pq.offer(new Pair(num,map.get(num)));
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> b.freq - a.freq);
+
+        for(int key : map.keySet()){
+            pq.offer(new Pair(key , map.get(key)));
         }
 
         int[] arr = new int[k];
 
-        for(int i =0 ;i < k ;i++){
-            arr[i] = pq.poll().element ;
+        for(int i = 0; i < k ; i++){
+            arr[i] = pq.poll().element;
         }
 
-        return arr ;
-
+        return arr;
     }
 }
